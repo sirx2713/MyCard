81730d6f1c9b8808d6fa9a81b5df5a50ce8b4a4b
sirx2713 <bradx2713@gmail.com>
2024-03-29 16:03:26 +0200

    Initial Commit

---
+++ MyCard/MyCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCardApp.swift
+//  MyCard
+//
+//  Created by Tafadzwa Alexander Razaro on 2024/03/29.
+//
+
+import SwiftUI
+
+@main
+struct MyCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
